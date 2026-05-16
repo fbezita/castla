297383a9560ff57ddeee55ee6ed41d04d50eaef7
fbezita <fbezita@gmail.com>
2026-05-17 00:25:17 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 11
-        versionName = "1.1.0"
+        versionName = "1.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
