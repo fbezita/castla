42aac04e10038faa5000543024d3f4f1b1e583ea
fbezita <fbezita@gmail.com>
2026-05-28 21:16:25 +0900

    Update build.gradle.kts
    bump version to 2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 11
-        versionName = "2.0.1"
+        versionName = "2.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
