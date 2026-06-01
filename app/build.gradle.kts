455c255a74886e01465d718329744d24faf1421f
fbezita <fbezita@gmail.com>
2026-06-01 23:42:08 +0900

    bump version to 2.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 12
-        versionName = "2.2.1"
+        versionName = "2.2.2"
         buildConfigField("String", "BUILD_TIMESTAMP", "\"$buildTimestamp\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
