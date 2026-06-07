bb3e6a9deea73177814cef0c410de2755abf2147
fbezita <fbezita@gmail.com>
2026-06-07 10:04:30 +0900

    bump version to 2.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 12
-        versionName = "2.4.1"
+        versionName = "2.4.2"
         buildConfigField("String", "BUILD_TIMESTAMP", "\"$buildTimestamp\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
